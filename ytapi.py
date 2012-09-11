28255b6558a12832d84ef4c193c947616ea418e7
zorro <zorro>
2012-09-11 20:20:03 +0000

    done python2 version

--- ytapi.py
+++ ytapi.py
@@ -1,21 +1,22 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
 import re, sys, json
-from urllib import request
+import urllib2 as request
+from urllib import pathname2url
 
 YANDEX_TRANSLATE_JSON = "http://translate.yandex.net/api/v1/tr.json/translate?lang="
 
 def get_translate(for_translate, trans_type='en'):
     global YANDEX_TRANSLATE_JSON
     result = False
-    prepate_url = request.pathname2url(for_translate)
+    prepate_url = pathname2url(for_translate)
     trans_types = {'en': 'en-ru', 'ru': 'ru-en'}
     prepate_url = YANDEX_TRANSLATE_JSON + trans_types[trans_type] + "&text=" + prepate_url
     try:
         conn = request.urlopen(prepate_url)
-        if conn.status == 200:
-            from_url = conn.read().decode('utf-8')
+        if conn.code == 200:
+            from_url = conn.read()
             result = json.loads(from_url)
     except Exception as e:
         print("Not connection\nError: ".format(e))
@@ -28,7 +29,7 @@ def main():
     args = sys.argv[1:]
     arg = get_translate(" ".join(args))
     if arg:
-        print(arg['text'])
+        print(arg['text'][0])
     else:
         print("Error")
     return 0
