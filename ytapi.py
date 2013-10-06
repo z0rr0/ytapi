1887f914bab057cab96e95e40b6743396f8d7d20
Alexander Zaytsev <thebestzorro@yandex.ru>
2013-10-06 13:02:18 +0400

    context manager for connection was added

--- ytapi.py
+++ ytapi.py
@@ -20,37 +20,35 @@ def get_dict(for_dict, dict_type):
     prepate_url = parse.urlencode(params, encoding="utf-8")
     if key.DEBUG: print(YANDEX_DICT_JSON + prepate_url)
     try:
-        conn = request.urlopen(YANDEX_DICT_JSON + prepate_url, None, 1)
-        if conn.status == 200:
-            from_url = conn.read().decode('utf-8')
-            result = json.loads(from_url)
-            # prepare result
-            all_result = []
-            for d in result['def']:
-                ts = " [{0}] ".format(d['ts']) if 'ts' in d.keys() else " "
-                txt_result = u"{0}{1}({2})\n".format(d['text'], ts, d['pos'])
-                ar_result = []
-                for res in d['tr']:
-                    keys = res.keys()
-                    syn, mean, ex = "", "", ""
-                    if 'syn' in keys:
-                        syn = "\n\tsyn: " + ", ".join([s['text'] for s in res['syn']])
-                    if 'mean' in keys:
-                        mean = "\n\tmean: " + ", ".join([s['text'] for s in res['mean']])
-                    if 'ex' in keys:
-                        ex = "\n\texamples:\n\t\t" + "\n\t\t".join(["{0}: {1}".format(s['text'], ", ".join([t["text"] for t in s['tr']])) for s in res['ex']])
-                    ar_result.append("\t{0} ({1}){2}{3}{4}".format(res['text'], res['pos'], syn, mean, ex))
-                all_result.append(txt_result + "\n".join(ar_result))
-            result = "\n".join(all_result)
-        else:
-            print("ERROR: connection answer code - {0}".format(conn.status))
-    except ValueError as e:
+        with request.urlopen(YANDEX_DICT_JSON + prepate_url, None, 1) as conn:
+            if conn.status == 200:
+                from_url = conn.read().decode('utf-8')
+                result = json.loads(from_url)
+                # prepare result
+                all_result = []
+                for d in result['def']:
+                    ts = " [{0}] ".format(d['ts']) if 'ts' in d.keys() else " "
+                    txt_result = u"{0}{1}({2})\n".format(d['text'], ts, d['pos'])
+                    ar_result = []
+                    for res in d['tr']:
+                        keys = res.keys()
+                        syn, mean, ex = "", "", ""
+                        if 'syn' in keys:
+                            syn = "\n\tsyn: " + ", ".join([s['text'] for s in res['syn']])
+                        if 'mean' in keys:
+                            mean = "\n\tmean: " + ", ".join([s['text'] for s in res['mean']])
+                        if 'ex' in keys:
+                            ex = "\n\texamples:\n\t\t" + "\n\t\t".join(["{0}: {1}".format(s['text'], ", ".join([t["text"] for t in s['tr']])) for s in res['ex']])
+                        ar_result.append("\t{0} ({1}){2}{3}{4}".format(res['text'], res['pos'], syn, mean, ex))
+                    all_result.append(txt_result + "\n".join(ar_result))
+                result = "\n".join(all_result)
+            else:
+                print("ERROR: connection answer code - {0}".format(conn.status))
+    except Exception as e:
         print("ERROR: {0}".format(e))
         return result
     else:
         if key.DEBUG: print(from_url)
-    # finally:
-    #     conn.close()
     return result
 
 def get_translate(for_translate, trans_type='en'):
@@ -69,19 +67,17 @@ def get_translate(for_translate, trans_type='en'):
     prepate_url = parse.urlencode(params, encoding="utf-8")
     if key.DEBUG: print(YANDEX_TRANSLATE_JSON + prepate_url)
     try:
-        conn = request.urlopen(YANDEX_TRANSLATE_JSON + prepate_url, None, 1)
-        if conn.status == 200:
-            from_url = conn.read().decode('utf-8')
-            result = json.loads(from_url)
-        else:
-            print("ERROR: connection answer code - {0}".format(conn.status))
+        with request.urlopen(YANDEX_TRANSLATE_JSON + prepate_url, None, 1) as conn:
+            if conn.status == 200:
+                from_url = conn.read().decode('utf-8')
+                result = json.loads(from_url)
+            else:
+                print("ERROR: connection answer code - {0}".format(conn.status))
     except Exception as e:
         print("ERROR: {0}".format(e))
         return result
     else:
         if key.DEBUG: print(from_url)
-    # finally:
-    #     conn.close()
     return result["text"]
 
 def check_spell(for_spelling, spell_type='en'):
@@ -100,19 +96,17 @@ def check_spell(for_spelling, spell_type='en'):
         "ERROR_CAPITALIZATION",
         "ERROR_TOO_MANY_ERRORS")
     try:
-        conn = request.urlopen(YANDEX_SPELL_JSON + prepate_url, None, 1)
-        if conn.status == 200:
-            from_url = conn.read().decode('utf-8')
-            result = json.loads(from_url)
-        else:
-            print("ERROR: connection answer code - {0}".format(conn.status))
+        with request.urlopen(YANDEX_SPELL_JSON + prepate_url, None, 1) as conn:
+            if conn.status == 200:
+                from_url = conn.read().decode('utf-8')
+                result = json.loads(from_url)
+            else:
+                print("ERROR: connection answer code - {0}".format(conn.status))
     except Exception as e:
         print("ERROR: {0}".format(e))
         return 1
     else:
         if key.DEBUG: print(from_url)
-    # finally:
-    #     conn.close()
     if len(result) > 0:
         print("Spelling: ", end="")
         for_result = []
